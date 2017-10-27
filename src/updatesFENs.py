f5fc4ca242fe492f78a084306fd1cc5543f32b12
shane-co <seamuss.mck@gmail.com>
2017-10-27 10:50:34 -0700

    adds code to update fens after moves, missing library currently

---
+++ src/updatesFENs.py
@@ -0,0 +1,35 @@
+import chess
+
+fenDocument = open('fork.csv','r')
+writeToDocument = open('updatedFENs.csv', 'r+')
+i = 0
+for line in fenDocument.read().split('\n'):
+    if i != 0:  
+        # read one line of fen document
+        # read moves, seperate them into two positions
+        # create board from position of first fen
+        # input first move
+        # output fen after first move and write it to updatedFENs
+        # add comma
+        # add next move to board
+        # write new fen board to csv file
+        fen = line.split(',')
+        # print (fen[3])
+        if fen[3] == " white":
+            fen[0] = fen[0] + ' w' + " KQkq - 0 1"
+        else:
+            fen[0] = fen[0] + ' b' + " KQkq - 0 1"
+        board = chess.Board(fen[0])
+        # print (fen[1]+fen[2])
+        # FirstMove = chess.Move.from_uci(fen[1]+fen[2])
+        for char in ' ':
+            fen[1] = fen[1].replace(char,'')
+            fen[2] = fen[2].replace(char, '')
+        board.push_san(fen[1])
+        newFen = board.fen()
+        writeToDocument.write(newFen + ',')
+        board.push_san(fen[2])
+        newFen = board.fen()
+        writeToDocument.write(newFen + '\n')
+    i +=1
+writeToDocument.close()
